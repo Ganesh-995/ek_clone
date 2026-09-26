9122244f8a0ab3520f79a59050c6b7a6acab80ac
Ganesh Rathore <ganeshrathore518@gmail.com>
2026-09-26 10:42:34 +0530

    Sync detail routes with browser URL

--- app/AppRouterShell.jsx
+++ app/AppRouterShell.jsx
@@ -1,30 +1,15 @@
 'use client';
 
-import { Suspense } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
-import { MemoryRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../src/App';
 import { ProductProvider } from '../src/context/ProductContext';
 
-function AppRouterShellContent() {
-  const pathname = usePathname() || '/';
-  const searchParams = useSearchParams();
-  const search = searchParams.toString();
-  const initialEntry = search ? `${pathname}?${search}` : pathname;
-
+export default function AppRouterShell() {
   return (
-    <MemoryRouter key={initialEntry} initialEntries={[initialEntry]} initialIndex={0}>
+    <BrowserRouter>
       <ProductProvider>
         <App />
       </ProductProvider>
-    </MemoryRouter>
-  );
-}
-
-export default function AppRouterShell() {
-  return (
-    <Suspense fallback={null}>
-      <AppRouterShellContent />
-    </Suspense>
+    </BrowserRouter>
   );
 }
